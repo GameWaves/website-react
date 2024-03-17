015e3eaf17ae187b6683fd4ff474f2f6db9bfb5c
Louis Gallet <me@louisgallet.fr>
2024-03-18 00:40:04 +0100

    feat: :sparkles:  Change meta description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,8 +32,8 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`A friend share you ${siteConfig.title}`}
+      description="He's done the right thing, click, you're in for a real treat!">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
